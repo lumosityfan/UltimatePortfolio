236e1b67095dd958ecbc342d7039fca2ac894660
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-04-12 10:04:35 -0500

    Created UltimatePortfolio project
    The UltimatePortfolio project will be based on the Ultimate Portfolio App series on Hacking with Swift+ by Paul Hudson.

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Jeff Xie on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
